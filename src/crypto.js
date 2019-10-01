31e174a18e8ab2da8e0b78c2fc69fd89ff891cf3
gilardh <gilardh@yahoo.com>
2019-10-02 01:13:03 +0200

    Update crypto.js

--- src/crypto.js
+++ src/crypto.js
@@ -1,8 +1,15 @@
 var createHash = require('create-hash')
-var groestlhash = require('groestl-hash-js')
+var crypto = require('crypto')
 
 function ripemd160 (buffer) {
-  return createHash('rmd160').update(buffer).digest()
+  var hash = 'rmd160'
+  var supportedHashes = crypto.getHashes()
+  // some environments (electron) only support the long alias
+  if (supportedHashes.indexOf(hash) === -1 && supportedHashes.indexOf('ripemd160') !== -1) {
+    hash = 'ripemd160'
+  }
+
+  return createHash(hash).update(buffer).digest()
 }
 
 function sha1 (buffer) {
@@ -21,15 +28,10 @@ function hash256 (buffer) {
   return sha256(sha256(buffer))
 }
 
-function groestl (buffer) {
-  return Buffer(groestlhash.groestl_2(buffer, 1, 1))
-}
-
 module.exports = {
   hash160: hash160,
   hash256: hash256,
   ripemd160: ripemd160,
   sha1: sha1,
-  sha256: sha256,
-  groestl: groestl
+  sha256: sha256
 }
